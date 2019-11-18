5db3279f1ed17918083e73f703a4180ece22df71
srad <ssedighi@posteo.de>
2019-11-18 18:55:09 +0100

    Adding route /client/stats to query the data of *one* user.

--- routes/client.js
+++ routes/client.js
@@ -2,6 +2,32 @@ const express = require("express");
 const router = express.Router();
 const connect = require("../services/mongoose");
 
+router.get("/stats", (req, res, next) => {
+  connect.then(({Client, Answer}) => {
+    const token = req.headers.authorization.split(" ")[1];
+    Client.findOne({client: token})
+      .then(client => {
+        Answer.aggregate([
+          {$match: {client: client._id}},
+          {$group: {_id: "$client", total: {$sum: "$score"}}},
+          {$limit: 10},
+          {$project: {_id: false, total: true, client: {name: client.name}}},
+          {$sort: {total: -1}},
+        ]).exec((error, result) => {
+          if (error) {
+            res.status(400).send({error});
+            return;
+          }
+          result.client = {name: client.name};
+          res.send(result);
+        });
+      }).catch(error => {
+        res.status(400).send({error});
+        next();
+      });
+  });
+});
+
 router.get("/top", function (req, res, next) {
   connect.then(models => {
     models.Answer.aggregate([
